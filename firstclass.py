d2f970bcc33dc0993d1547a0884691be8aeeb44b
Ammahil <hilda.amaboahemaah@dawsoncollege.qc.ca>
2024-08-30 23:35:37 +0000

    first assignment

---
+++ firstclass.py
@@ -0,0 +1,16 @@
+print("Hello word")
+print(78-9)
+print("78-9")
+age=20
+print("my age")
+print("age")
+print (age)
+age= input("what is your age")
+print(age)
+print(age)
+firstname = input("enter your first name")
+lastname = input ("enter your last name")
+print(firstname + " " + lastname)
+print(firstname + " " lastname)
+
+
---
+++ maths.py
@@ -0,0 +1,5 @@
+twotothepowerof5=2**5
+print(twotothepowerof5)
+formular=5*4/2+9-8
+print(formular)
+
